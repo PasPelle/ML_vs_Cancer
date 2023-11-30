d297f6eb911279a2d9d7a1009b9f1dbc01886fc2
Katazyna Balcevic <katazyna.balcevic@gmail.com>
2023-11-30 17:27:46 +0100

    Clean up

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ import streamlit as st
 import requests
 from PIL import Image
 import numpy as np
-from io import BytesIO
-from ML_vs_Cancer.ml_logic.read_tiff_image import read_tiff_image
-import cv2
 
 # Use HTML style tags to define custom styles
 st.markdown('''
@@ -37,7 +34,7 @@ image_placeholder = st.empty()
 # Upload_file:
 uploaded_file = st.file_uploader("Choose an image...", type=["tif", "tiff"])
 if uploaded_file is not None:
-    st.image(uploaded_file, use_column_width=True)
+    st.image(uploaded_file, width=300, caption="Uploaded image")
     if st.button('Get Prediction'):
         img = uploaded_file.getvalue()
         files={"file": ("image.tiff", img, "image/tiff")}
@@ -49,61 +46,18 @@ if uploaded_file is not None:
                 if prediction is not None:
                     pred = prediction * 100
                     if prediction < 0.5:
+                        pred = 100 - pred
                         st.markdown(f"<h2 style='color:red;'>Malignant</h2>", unsafe_allow_html=True)
-                        st.markdown(f"<h3 style='color:black;'> Accuracy: {pred}%</h3>", unsafe_allow_html=True)
+                        st.markdown(f"<h3 style='color:black;'>Probability: {pred}%</h3>", unsafe_allow_html=True)
                     else:
                         st.markdown(f"<h2 style='color:green;'>Non-Malignant</h2>", unsafe_allow_html=True)
-                        st.markdown(f"<h3 style='color:black;'>Accuracy: {pred}%</h3>", unsafe_allow_html=True)
+                        st.markdown(f"<h3 style='color:black;'>Probability: {pred}%</h3>", unsafe_allow_html=True)
                 else:
                     st.error("Error: Prediction value is None.")
             else:
                 st.error("Error: 'Prediction' key not found in API response.")
         else:
             st.error(f"Error in API request. Status code: {response.status_code}")
-
-
-
-# Upload Image
-# uploaded_file = st.file_uploader("Choose an image...", type=["tif", "tiff"])
-# if uploaded_file is not None:
-#     try:
-#         image = Image.open(uploaded_file)
-#         image_placeholder.image(image, caption='Uploaded Image', use_column_width=True)
-
-#         if st.button('Get Prediction'):
-#             file_content = uploaded_file.read()
-#             try:
-#                 if file_content:
-#                     response = requests.post(
-#                         url,
-#                         files={"img": ("image.tiff", file_content, "image/tiff")}
-#                     )
-
-#                     if response.status_code == 200:
-#                         result = response.json()
-
-#                         if image is not None:
-#                             image_placeholder.image(image, caption='Uploaded Image', use_column_width=True)
-
-#                         if "Prediction" in result:
-#                             prediction = result["Prediction"]
-#                             if prediction is not None:
-#                                 if prediction < 0.5:
-#                                     st.markdown(f"<h2 style='color:red;'>Malignant</h2>", unsafe_allow_html=True)
-#                                 else:
-#                                     st.markdown(f"<h2 style='color:green;'>Non-Malignant</h2>", unsafe_allow_html=True)
-#                             else:
-#                                 st.error("Error: Prediction value is None.")
-#                         else:
-#                             st.error("Error: 'Prediction' key not found in API response.")
-#                     else:
-#                         st.error(f"Error in API request. Status code: {response.status_code}")
-#                 else:
-    #                 st.error("Error: Empty file content. Please upload a valid image.")
-    #         except Exception as e:
-    #             st.error(f"Error processing image: {e}")
-    # except Exception as e:
-    #     st.error(f"Error processing image: {e}")
 
 # Footer
 st.markdown('<p class="big-font">Thank you for using the Histopathologic Cancer Detection app!</p>', unsafe_allow_html=True)
